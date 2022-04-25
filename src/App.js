e06a642f355b9bd03417ce0704454d58673c683b
nannabergquist <bergquistnanna@gmail.com>
2022-04-25 14:02:36 +0200

    #10 done with page 2 + css

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import './App.css';
 import Navbar from "./components/navbar";
 import Role from './pages/Role';
 import Start from './pages/Start';
+import Artifacts from './pages/Artifacts';
+import Sprint from './pages/Sprint';
+import Estimation from './pages/Estimation';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* <Navbar /> */}
-        <Route path="/" element={<Start />} />
-        <Route path="/Role" element={<Role />} />
-      </Routes>
-    </Router>
+    <>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Start />} />
+          <Route path="/Role" element={<Role />} />
+          <Route path="/Artifacts" element={<Artifacts />} />
+          <Route path="/Sprint" element={<Sprint />} />
+          <Route path="/Estimation" element={<Estimation />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
